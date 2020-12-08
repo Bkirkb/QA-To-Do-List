4567bb76a102e2fac996b25a7c79e88f833492a0
Bkirkb <b.kirkby489@gmail.com>
2020-12-08 16:32:27 +0000

    struggling with jinja

--- application/__init__.py
+++ application/__init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@35.234.138.197/tdl"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 db = SQLAlchemy(app)
 
 from application import routes
--- application/models.py
+++ application/models.py
@@ -6,4 +6,4 @@ class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(50), nullable=False)
     complete = db.Column(db.Boolean, nullable=False, default=False)
-    date_created = db.Column(db.DateTime, nullable=False, default=)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.now())
